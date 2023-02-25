0160d9b3898b155860b6badd54cde1dcb69053da
Harry Singh <2645294S@student.gla.ac.uk>
2023-02-25 21:10:41 +0000

    slight changes

--- dailydoodle/urls.py
+++ dailydoodle/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from django.urls import include
 from dailydoodle import views
 
+app_name = "dailydoodle"
+
 urlpatterns = [
     path("",views.Index.as_view(),name="index"),
 ]
